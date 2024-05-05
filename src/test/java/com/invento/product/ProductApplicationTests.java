7389d7cd12b3de7f7e14dfb8c7cb0831990368f1
SagarSisodiya <sagarsisodiya8@gmail.com>
2024-05-05 17:36:10 +0530

    updated test package name

--- src/test/java/com/invento/product/ProductApplicationTests.java
+++ src/test/java/com/invento/product/ProductApplicationTests.java
@@ -1,10 +1,10 @@
-package com.invento.customer;
+package com.invento.product;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CustomerApplicationTests {
+class ProductApplicationTests {
 
 	@Test
 	void contextLoads() {
