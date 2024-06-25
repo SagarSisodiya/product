4b66db5a9a78fb27668c3fc40d347d872468ee1b
SagarSisodiya <sagarsisodiya8@gmail.com>
2024-06-26 03:49:10 +0530

    removed temp changes

--- src/main/java/com/invento/product/controller/ProductController.java
+++ src/main/java/com/invento/product/controller/ProductController.java
@@ -3,7 +3,6 @@ package com.invento.product.controller;
 import java.util.List;
 
 import org.bson.Document;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -30,9 +29,6 @@ import com.invento.product.util.Constants;
 @RestController
 @RequestMapping(path="/product", produces= {MediaType.APPLICATION_JSON_VALUE}	)
 public class ProductController {
-	
-	@Value("${spring.data.mongodb.database}")
-	private String database;
 
 	private ProductService productService;
 
