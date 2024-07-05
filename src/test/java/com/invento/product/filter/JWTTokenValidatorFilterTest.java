5c38b00efa2e5a0e4df86b4a85c69751768d7411
SagarSisodiya <sagarsisodiya8@gmail.com>
2024-07-06 02:16:05 +0530

    Added filter test

--- src/test/java/com/invento/product/filter/JWTTokenValidatorFilterTest.java
+++ src/test/java/com/invento/product/filter/JWTTokenValidatorFilterTest.java
@@ -20,6 +20,20 @@ import jakarta.servlet.http.HttpServletResponse;
 public class JWTTokenValidatorFilterTest {
 	
 	@Test
+	public void testDoFilter() throws ServletException, IOException {
+		
+		JWTTokenValidatorFilter filter = new JWTTokenValidatorFilter();
+		 
+        HttpServletRequest mockReq = Mockito.mock(HttpServletRequest.class);
+        HttpServletResponse mockResp = Mockito.mock(HttpServletResponse.class);
+        FilterChain mockFilterChain = Mockito.mock(FilterChain.class);
+        Mockito.when(mockReq.getRequestURI()).thenReturn("/");
+        Mockito.when(mockReq.getHeader(Mockito.anyString())).thenReturn(TestConstants.MOCK_JWT_TOKEN);
+      	filter.doFilter(mockReq, mockResp, mockFilterChain);
+        filter.destroy();
+	}
+	
+	@Test
 	public void testDoFilter_JwtException() throws ServletException, IOException {
 		
 		JWTTokenValidatorFilter filter = new JWTTokenValidatorFilter();
@@ -59,7 +73,7 @@ public class JWTTokenValidatorFilterTest {
         FilterChain mockFilterChain = Mockito.mock(FilterChain.class);
         when(mockReq.getRequestURI()).thenReturn("/");
         when(mockReq.getHeader(TestConstants.JWT_HEADER))
-        .thenReturn(TestConstants.MOCK_JWT_TOKEN);
+        .thenReturn(TestConstants.INVALID_MOCK_JWT_TOKEN);
         
         Assertions.assertThrows(SignatureException.class, 
         	() -> filter.doFilter(mockReq, mockResp, mockFilterChain));
@@ -76,7 +90,7 @@ public class JWTTokenValidatorFilterTest {
         FilterChain mockFilterChain = Mockito.mock(FilterChain.class);
         when(mockReq.getRequestURI()).thenReturn("/");
         when(mockReq.getHeader(TestConstants.JWT_HEADER))
-        .thenReturn(TestConstants.BEARER +  TestConstants.MOCK_JWT_TOKEN);
+        .thenReturn(TestConstants.BEARER +  TestConstants.INVALID_MOCK_JWT_TOKEN);
         
         Assertions.assertThrows(SignatureException.class, 
         	() -> filter.doFilter(mockReq, mockResp, mockFilterChain));
--- src/test/java/com/invento/product/util/TestConstants.java
+++ src/test/java/com/invento/product/util/TestConstants.java
@@ -4,9 +4,7 @@ public class TestConstants {
 
 	public static final String PRODUCT_ID = "6639dd31e5b7490f597ee80f";
 	public static final String  JWT_HEADER = "Authorization";
-	public static final String  MOCK_JWT_TOKEN = 
-			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
-			+ ".eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ"
-			+ ".SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
-	public static final String BEARER = "Bearer";
+	public static final String  MOCK_JWT_TOKEN = "eyJhbGciOiJIUzM4NCJ9.eyJpc3MiOiJJbnZlbnRvIiwic3ViIjoiSldUIFRPS0VOIiwidXNlcm5hbWUiOiJUZXN0IFVzZXIiLCJhdXRob3JpdGllcyI6Ik9JRENfVVNFUixTQ09QRV9vcGVuaWQsU0NPUEVfZW1haWwsUk9MRV9JTlZFTlRPX1JPLFNDT1BFX3Byb2ZpbGUiLCJpYXQiOjE3MjAyMTE2NzEsImV4cCI6MTcyMDI0MTY3MX0.brra-c9lQWWKl4B2zL0LeyZBO9tHUIyFql4QyTWKDBgOZXydVC39C6NFCUQGQnGQ";
+	public static final String  INVALID_MOCK_JWT_TOKEN = "eyJhbGciOiJIUzM4NCJ9.eyJpc3MiOiJJbnZlbnRvIiwic3ViIjoiSldUIFRPS0VOIiwidXNlcm5hbWUiOiJUZXN0IFVzZXIiLCJhdXRob3JpdGllcyI6Ik9JRENfVVNFUixTQ09QRV9vcGVuaWQsU0NPUEVfZW1haWwsUk9MRV9JTlZFTlRPX1JPLFNDT1BFX3Byb2ZpbGUiLCJpYXQiOjE3MjAyMTE2NzEsImV4cCI6MTcyMDI0MTY3MX0.brra-c9lQWWKl4B2zL0LeyZBO9tHUIyFql4QyTWKDBgOZXydVC39C6NFCUQGQnG";
+	public static final String BEARER = "Bearer ";
 }
