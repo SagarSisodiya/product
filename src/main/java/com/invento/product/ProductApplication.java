043db4ba64c470213168dddb5ad0cc2ee64e2a9a
SagarSisodiya <sagarsisodiya8@gmail.com>
2024-07-07 20:15:26 +0530

    Fixed test cases failure due to MongoDB Audit

--- src/main/java/com/invento/product/ProductApplication.java
+++ src/main/java/com/invento/product/ProductApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
-@SpringBootApplication
-//@EnableMongoAuditing 
+@SpringBootApplication 
 public class ProductApplication {
 
 	public static void main(String[] args) {
